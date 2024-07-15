87cbd45474287ad452e8934e9736ab424f87ad87
Tomasz Kurowski <tomasz.kurowski@non.se.com>
2024-07-15 18:17:33 +0200

    Code Cleanings: utils/locales

---
+++ src/utils/locales.ts
@@ -0,0 +1,35 @@
+export const duplicateDefaultValuesToLocaleValues = (blocks: any[], locale: string): any[] => {
+  const defaultLocale = 'Default';
+  const traverseOptions = (options: any) => {
+    for (const key in options) {
+      if (options.hasOwnProperty(key)) {
+        const value = options[key];
+        if (value && typeof value === 'object' && value["@type"] === "@builder.io/core:LocalizedValue") {
+          if (!value[locale] && value[defaultLocale]) {
+            value[locale] = value[defaultLocale];
+          }
+        } else if (value && typeof value === 'object') {
+          traverseOptions(value);
+        } else if (Array.isArray(value)) {
+          value.forEach(item => {
+            if (typeof item === 'object') {
+              traverseOptions(item);
+            }
+          });
+        }
+      }
+    }
+  };
+
+  return blocks.map(block => {
+    if (block.component && block.component.options) {
+      traverseOptions(block.component.options);
+    }
+    return block;
+  });
+};
+
+export const getLocaleFromPage = (page: any): string => {
+  const localeQuery = page.query.find((q: any) => q.property === "locale");
+  return localeQuery ? localeQuery.value[0] : null;
+};
